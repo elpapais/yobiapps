c63acc02df1634d135e06be829c2e94fb7bd5fda
Sripathi <sripathi@primechain.in>
2017-03-28 18:15:45 +0530

    Made changes to YobiWallet UI

--- helperFunctions.php
+++ helperFunctions.php
@@ -417,7 +417,7 @@
 			}
 
 			$printDetails .= "<tr>";
-			$printDetails .= "<td>".date('d-m-Y'.', '.'h:i:s a', $time)."</td>";
+			$printDetails .= "<td><span>".date('d-m-Y'.', '.'h:i:s a', $time)."</span></td>";
 			$printDetails .= "<td>".(($senderAddress==$userAddress) ? "You" : $senderAddress)."</td>";
 			$printDetails .= "<td>".(($recipientAddress==$userAddress) ? "You" : $recipientAddress)."</td>";
 			$printDetails .= "<td>".abs(floatval($amount))."</td>";
